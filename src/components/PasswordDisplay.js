b7af7431b3d31ab2f7fc2f6974dd24be995f2125
jxhnkndl <jkroyston@gmail.com>
2023-06-06 11:04:57 -0400

    create placeholder password form and password display components

---
+++ src/components/PasswordDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PasswordDisplay() {
+  return <div className="flex justify-center py-8">PASSWORD DISPLAY</div>;
+}
+
+export default PasswordDisplay;
---
+++ src/components/PasswordForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function PasswordForm() {
+  return <section className="flex justify-center py-8">PASSWORD FORM</section>;
+}
+
+export default PasswordForm;
