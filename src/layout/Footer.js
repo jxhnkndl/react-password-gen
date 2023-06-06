becd5a302ccf5dc8acbef842653b9322eb6b037a
jxhnkndl <jkroyston@gmail.com>
2023-06-06 11:04:34 -0400

    create placeholder header and footer components

---
+++ src/layout/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Footer() {
+  return <footer className="px-6 py-6">FOOTER</footer>;
+}
+
+export default Footer;
---
+++ src/layout/Header.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Header() {
+  return <header className="px-6 py-6">HEADER</header>;
+}
+
+export default Header;
