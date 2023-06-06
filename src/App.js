e825f31fcbe39d1037f2ea13f69a72bc87afaedd
jxhnkndl <jkroyston@gmail.com>
2023-06-06 11:05:08 -0400

    create basic mobile layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import Header from './layout/Header';
+import Footer from './layout/Footer';
+import PasswordDisplay from './components/PasswordDisplay';
+import PasswordForm from './components/PasswordForm';
 
 function App() {
-  return <h1>keygen</h1>;
+  return (
+    <div className="min-h-screen flex flex-col justify-between">
+      <Header />
+      <main className="container px-6">
+        <PasswordDisplay />
+        <PasswordForm />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
