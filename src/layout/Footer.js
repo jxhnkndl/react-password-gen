f0251ab640a44a5a6dfc7574f5233fe21470dc8c
jxhnkndl <jkroyston@gmail.com>
2023-06-06 11:58:09 -0400

    adjust footer padding

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Footer() {
-  return <footer className="px-6 py-6">FOOTER</footer>;
+  return <footer className="px-6 py-3">FOOTER</footer>;
 }
 
 export default Footer;
