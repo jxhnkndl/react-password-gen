56f007e026314bab63c1aa60f963876e87374513
jxhnkndl <jkroyston@gmail.com>
2023-06-06 12:06:09 -0400

    add footer content

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { FaReact } from 'react-icons/fa';
 
 function Footer() {
-  return <footer className="px-6 py-3">FOOTER</footer>;
+  return (
+    <footer className="px-6 py-3">
+      <FaReact className='inline mr-2 mb-1' />
+      <p className='inline'>Made with React + Tailwind CSS</p>
+    </footer>
+  );
 }
 
 export default Footer;
