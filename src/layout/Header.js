8dccc8f9397594c9910eb6b5322d1f69e704cc40
jxhnkndl <jkroyston@gmail.com>
2023-06-06 11:57:27 -0400

    add header title and icon

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { FaShieldAlt } from 'react-icons/fa';
 
 function Header() {
-  return <header className="px-6 py-6">HEADER</header>;
+  return (
+    <header className="px-6 py-6 flex align-center text-xl text-primary font-semibold">
+      <FaShieldAlt className="inline text-2xl mr-2" />
+      <p className='inline'>PASSGEN</p>
+    </header>
+  );
 }
 
 export default Header;
