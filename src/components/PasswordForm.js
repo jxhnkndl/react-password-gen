d5b483d468731b5b1492e5ad01309a8752559792
jxhnkndl <jkroyston@gmail.com>
2023-06-07 19:22:11 -0400

    create password options state and change handler for option toggles

--- src/components/PasswordForm.js
+++ src/components/PasswordForm.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function PasswordForm() {
+  const [length, setLength] = useState(0);
+  const [options, setOptions] = useState({
+    lowercase: false,
+    uppercase: false,
+    numbers: false,
+    specialChars: false,
+  });
+
+  const handleCheck = (event) => {
+    const { name, checked } = event.target;
+
+    console.log(name, checked);
+
+    setOptions({
+      ...options,
+      [name]: checked,
+    });
+  };
+
   return (
     <section className="py-8">
-      {/* password length slider */}
+      {/* password length */}
       <div className="form-control px-1 mb-8">
         <p className="mb-3">LENGTH</p>
         <input
@@ -14,40 +33,55 @@ function PasswordForm() {
           className="range range-primary transform scale-y-105"
         />
       </div>
-      {/* character option toggles */}
+      {/* lowercase letters */}
       <div className="form-control my-3">
         <label className="label cursor-pointer">
           <span className="">LOWERCASE LETTERS</span>
           <input
+            name="lowercase"
             type="checkbox"
             className="toggle h-6 w-12 transform scale-125 mr-1"
+            checked={options.lowercase}
+            onChange={handleCheck}
           />
         </label>
       </div>
+      {/* uppercase letters */}
       <div className="form-control my-3">
         <label className="label cursor-pointer">
           <span className="">UPPERCASE LETTERS</span>
           <input
+            name="uppercase"
             type="checkbox"
             className="toggle h-6 w-12 transform scale-125 mr-1"
+            checked={options.uppercase}
+            onChange={handleCheck}
           />
         </label>
       </div>
+      {/* numbers */}
       <div className="form-control my-3">
         <label className="label cursor-pointer">
           <span className="">NUMBERS</span>
           <input
+            name="numbers"
             type="checkbox"
             className="toggle h-6 w-12 transform scale-125 mr-1"
+            checked={options.numbers}
+            onChange={handleCheck}
           />
         </label>
       </div>
+      {/* special characters */}
       <div className="form-control my-3">
         <label className="label cursor-pointer">
           <span className="">SPECIAL CHARACTERS</span>
           <input
+            name="specialChars"
             type="checkbox"
             className="toggle h-6 w-12 transform scale-125 mr-1"
+            checked={options.specialChars}
+            onChange={handleCheck}
           />
         </label>
       </div>
