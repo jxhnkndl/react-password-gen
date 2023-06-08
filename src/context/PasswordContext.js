bc6e329081064ed839b4b2a068e77343fd2b95c1
jxhnkndl <jkroyston@gmail.com>
2023-06-07 20:01:23 -0400

    create logic for creating password from user selected options

--- src/context/PasswordContext.js
+++ src/context/PasswordContext.js
@@ -1,23 +1,46 @@
 import { useState, createContext } from 'react';
+import {
+  lowercase,
+  uppercase,
+  numbers,
+  specialChars,
+} from '../utils/charTypes';
 
 // init password context
 const PasswordContext = createContext();
 
 // export password context provider
 export const PasswordProvider = ({ children }) => {
-  const [password, setPassword] = useState(0);
+  const [password, setPassword] = useState('');
 
   // create password
   const generatePassword = (length, options) => {
-    console.log(length);
-    console.log(options);
-  }
+    let charChoices = '';
+    let passwordChars = '';
+
+    // concatenate final char choices based on options
+    if (options.lowercase) charChoices = charChoices + lowercase;
+    if (options.uppercase) charChoices = charChoices + uppercase;
+    if (options.numbers) charChoices = charChoices + numbers;
+    if (options.specialChars) charChoices = charChoices + specialChars;
+
+    // generate password from final char string
+    for (let i = 0; i < length; i++) {
+      let randomIndex = Math.floor(Math.random() * charChoices.length);
+      let nextChar = charChoices[randomIndex];
+
+      passwordChars = passwordChars + nextChar;
+      console.log(passwordChars);
+    }
+
+    setPassword(passwordChars);
+  };
 
   return (
     <PasswordContext.Provider
       value={{
         password,
-        generatePassword
+        generatePassword,
       }}
     >
       {children}
