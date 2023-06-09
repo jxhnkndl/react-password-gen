d292c07343dd98c2183beb5da77d86bb3bbd3fa2
jxhnkndl <jkroyston@gmail.com>
2023-06-09 10:48:57 -0400

    add handler for copying password to clipboard when user taps or clicks password display

--- src/components/PasswordDisplay.js
+++ src/components/PasswordDisplay.js
@@ -1,14 +1,31 @@
 import React, { useContext } from 'react';
 import { FaUnlock } from 'react-icons/fa';
+import { FaLock } from 'react-icons/fa';
 import PasswordContext from '../context/PasswordContext';
 
 function PasswordDisplay() {
   const { password } = useContext(PasswordContext);
 
+  // copy password from ui when user taps/clicks display
+  const handleCopy = async () => {
+    if (password) {
+      try {
+        await navigator.clipboard.writeText(password);
+        console.log(`Copied Password: ${password}`);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   return (
-    <section className="py-8">
-      <div className="flex justify-center mb-6">
-        <FaUnlock className="text-9xl opacity-30" />
+    <section className="py-8" onClick={handleCopy}>
+      <div className="flex justify-center mb-10">
+        {password ? (
+          <FaLock className="text-9xl text-accent" />
+        ) : (
+          <FaUnlock className="text-9xl opacity-30" />
+        )}
       </div>
       {/* display password or placeholder depending on password state */}
       <div className="flex justify-center">
