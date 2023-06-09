ade62c93bce52a77c4f54231571a7184a82b6ccc
jxhnkndl <jkroyston@gmail.com>
2023-06-09 09:56:40 -0400

    config password context so that password can be displayed on page once generated

--- src/components/PasswordDisplay.js
+++ src/components/PasswordDisplay.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { FaUnlock } from 'react-icons/fa';
+import PasswordContext from '../context/PasswordContext';
 
 function PasswordDisplay() {
+  const { password } = useContext(PasswordContext);
+
   return (
     <section className="py-8">
-      <div className='flex justify-center mb-6'>
-        <FaUnlock className='text-9xl opacity-30' />
+      <div className="flex justify-center mb-6">
+        <FaUnlock className="text-9xl opacity-30" />
       </div>
-      <div className='flex justify-center'>
-        <p className='text-2xl text-center opacity-30'>
-          ** PASSWORD **
-        </p>
+      {/* display password or placeholder depending on password state */}
+      <div className="flex justify-center">
+        {password ? (
+          <p className="text-2xl text-center text-semibold text-accent-content">
+            {password}
+          </p>
+        ) : (
+          <p className="text-2xl text-center opacity-30">*** PASSWORD ***</p>
+        )}
       </div>
     </section>
   );
