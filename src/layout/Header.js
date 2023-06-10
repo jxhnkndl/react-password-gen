c758dd6ab6dff709be1c2506a37dd5ebbc5c406d
jxhnkndl <jkroyston@gmail.com>
2023-06-10 19:26:48 -0400

    add toggle switch to header for switching between light and dark mode

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -5,21 +5,39 @@ import ThemeContext from '../context/ThemeContext';
 function Header() {
   const { isDarkTheme, setIsDarkTheme } = useContext(ThemeContext);
 
+  let toggleText;
+
+  // set toggle switch label based on theme state
+  isDarkTheme ? (toggleText = 'Dark') : (toggleText = 'Light');
+
   // handle dark/light mode toggle switch
   const handleCheck = (event) => setIsDarkTheme(!isDarkTheme);
 
   return (
-    <header className="px-6 py-6 flex align-center justify-between text-xl text-accent font-semibold">
-      <div>
+    <header className="px-6 py-6 flex items-center justify-between text-xl text-accent font-semibold">
+      <div className='flex items-center'>
         <FaShieldAlt className="inline text-2xl mr-2" />
-        <p className="inline">PASSGEN</p>
+        <p className={`mb-0 inline ${!isDarkTheme && 'text-neutral'}`}>
+          PASSGEN
+        </p>
       </div>
-      <input
-        type="checkbox"
-        className="toggle toggle-accent"
-        onChange={handleCheck}
-        checked={isDarkTheme}
-      />
+      <div className="form-control">
+        <label className="label">
+          <span
+            className={`text-sm mr-2 ${
+              isDarkTheme ? 'text-base-content' : 'text-neutral'
+            }`}
+          >
+            {toggleText}
+          </span>
+          <input
+            type="checkbox"
+            className="toggle toggle-accent"
+            onChange={handleCheck}
+            checked={isDarkTheme}
+          />
+        </label>
+      </div>
     </header>
   );
 }
