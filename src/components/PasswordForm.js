df76d25141f682d9b149c3ede5c74fdb965e4b92
jxhnkndl <jkroyston@gmail.com>
2023-06-10 15:50:46 -0400

    add password length indicator and wrap form in card style

--- src/components/PasswordForm.js
+++ src/components/PasswordForm.js
@@ -62,85 +62,90 @@ function PasswordForm() {
 
   return (
     <section className="pt-4 pb-8">
-      {/* password length */}
-      <div className="form-control px-1 mb-8">
-        <p className="mb-3">LENGTH</p>
-        <input
-          type="range"
-          min="8"
-          max="32"
-          value={length}
-          onChange={handleLength}
-          className="range range-accent transform scale-y-105"
-        />
-      </div>
-      {/* lowercase letters */}
-      <div className="form-control my-3">
-        <label className="label cursor-pointer">
-          <span className="">LOWERCASE LETTERS</span>
+      <div className="card bg-base-100 shadow-xl px-4 py-6">
+        {/* password length */}
+        <div className="form-control px-1 mb-8">
+          <div className="flex justify-between mb-3">
+            <p>LENGTH</p>
+            <p>{length} Characters</p>
+          </div>
           <input
-            name="lowercase"
-            type="checkbox"
-            className="toggle toggle-accent h-6 w-12 transform scale-125 mr-1"
-            checked={options.lowercase}
-            onChange={handleCheck}
+            type="range"
+            min="8"
+            max="32"
+            value={length}
+            onChange={handleLength}
+            className="range range-accent transform scale-y-105"
           />
-        </label>
-      </div>
-      {/* uppercase letters */}
-      <div className="form-control my-3">
-        <label className="label cursor-pointer">
-          <span className="">UPPERCASE LETTERS</span>
-          <input
-            name="uppercase"
-            type="checkbox"
-            className="toggle toggle-accent h-6 w-12 transform scale-125 mr-1"
-            checked={options.uppercase}
-            onChange={handleCheck}
-          />
-        </label>
-      </div>
-      {/* numbers */}
-      <div className="form-control my-3">
-        <label className="label cursor-pointer">
-          <span className="">NUMBERS</span>
-          <input
-            name="numbers"
-            type="checkbox"
-            className="toggle toggle-accent h-6 w-12 transform scale-125 mr-1"
-            checked={options.numbers}
-            onChange={handleCheck}
-          />
-        </label>
-      </div>
-      {/* special characters */}
-      <div className="form-control my-3">
-        <label className="label cursor-pointer">
-          <span className="">SPECIAL CHARACTERS</span>
-          <input
-            name="specialChars"
-            type="checkbox"
-            className="toggle toggle-accent h-6 w-12 transform scale-125 mr-1"
-            checked={options.specialChars}
-            onChange={handleCheck}
-          />
-        </label>
-      </div>
-      <button
-        disabled={!validOptions && 'disabled'}
-        className={`btn btn-block mt-4 ${validOptions && 'btn-accent'}`}
-        onClick={handleSubmit}
-      >
-        Generate Password
-      </button>
-      {password && (
+        </div>
+        {/* lowercase letters */}
+        <div className="form-control my-3">
+          <label className="label cursor-pointer">
+            <span className="">LOWERCASE LETTERS</span>
+            <input
+              name="lowercase"
+              type="checkbox"
+              className="toggle toggle-accent h-6 w-12 transform scale-125 mr-1"
+              checked={options.lowercase}
+              onChange={handleCheck}
+            />
+          </label>
+        </div>
+        {/* uppercase letters */}
+        <div className="form-control my-3">
+          <label className="label cursor-pointer">
+            <span className="">UPPERCASE LETTERS</span>
+            <input
+              name="uppercase"
+              type="checkbox"
+              className="toggle toggle-accent h-6 w-12 transform scale-125 mr-1"
+              checked={options.uppercase}
+              onChange={handleCheck}
+            />
+          </label>
+        </div>
+        {/* numbers */}
+        <div className="form-control my-3">
+          <label className="label cursor-pointer">
+            <span className="">NUMBERS</span>
+            <input
+              name="numbers"
+              type="checkbox"
+              className="toggle toggle-accent h-6 w-12 transform scale-125 mr-1"
+              checked={options.numbers}
+              onChange={handleCheck}
+            />
+          </label>
+        </div>
+        {/* special characters */}
+        <div className="form-control my-3">
+          <label className="label cursor-pointer">
+            <span className="">SPECIAL CHARACTERS</span>
+            <input
+              name="specialChars"
+              type="checkbox"
+              className="toggle toggle-accent h-6 w-12 transform scale-125 mr-1"
+              checked={options.specialChars}
+              onChange={handleCheck}
+            />
+          </label>
+        </div>
         <button
-          className="btn btn-neutral btn-block mt-4"
-          onClick={handleClear}
+          disabled={!validOptions && 'disabled'}
+          className={`btn btn-block mt-4 ${validOptions && 'btn-accent'}`}
+          onClick={handleSubmit}
         >
-          Clear Password
+          Generate Password
         </button>
-      )}
+        {password && (
+          <button
+            className="btn btn-neutral btn-block mt-4"
+            onClick={handleClear}
+          >
+            Clear Password
+          </button>
+        )}
+      </div>
     </section>
   );
 }
