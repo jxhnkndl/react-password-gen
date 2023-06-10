a6ee25d6775e4c040ac73c6e513e919d3f0a3e60
jxhnkndl <jkroyston@gmail.com>
2023-06-10 16:11:20 -0400

    import google fonts and extend tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   // paths to all component files
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          'Roboto',
+          'Helvetica',
+          'ui-sans-serif',
+          'system-ui',
+          'sans-serif',
+        ],
+        mono: ['Roboto Mono', 'ui-monospace', 'SFMono-Regular', 'monospace'],
+      },
+    },
+  },
   // import daisyui component library
   plugins: [require('daisyui')],
-}
+};
